af84d73d5835e8f8af5b6cc294414cafa0dafeaa
ma16 <ma16@home>
2017-07-12 08:50:40 +0200

    move alignment to copy() ; add two words because of PWM defect

--- cc/Device/Ws2812b/BitStream.cc
+++ cc/Device/Ws2812b/BitStream.cc
@@ -2,6 +2,7 @@
 
 #include "BitStream.h"
 #include <cassert>
+#include <iostream> // debugging
 
 void Device::Ws2812b::BitStream::pushBit(bool bit,size_t n)
 {
@@ -15,12 +16,12 @@ void Device::Ws2812b::BitStream::pushWord(uint32_t grb)
     {
 	if (0 == (grb & mask))
 	{
-	    this->pushBit(1, this->ticks.t0h) ;
+	    this->pushBit(1,this->ticks.t0h) ;
 	    this->pushBit(0,this->ticks.t0l) ;
 	}
 	else
 	{
-	    this->pushBit(1, this->ticks.t1h) ;
+	    this->pushBit(1,this->ticks.t1h) ;
 	    this->pushBit(0,this->ticks.t1l) ;
 	}
     }
@@ -34,12 +35,11 @@ void Device::Ws2812b::BitStream::push(uint32_t grb,size_t n)
 
 std::vector<uint32_t> Device::Ws2812b::BitStream::copy() const
 {
-    assert(0 == (this->q.size() % 32)) ;
     auto n = (this->q.size()+31) / 32 ;
     std::vector<uint32_t> v ;
-    v.reserve(n) ;
+    v.reserve(n+2) ;
     unsigned i = 0 ;
-    uint32_t bits = 0 ; // compiler may complain if not init.
+    uint32_t bits = 0 ;
     for (auto bit : this->q)
     {
 	bits <<= 1 ;
@@ -49,6 +49,15 @@ std::vector<uint32_t> Device::Ws2812b::BitStream::copy() const
 	v.push_back(bits) ;
 	i = 0 ;
     }
+    if (i != 0)
+    {
+	// last word padded with zeros (extends ticks.res)
+	bits <<= (32-i) ;
+	v.push_back(bits) ;
+    }
+    // [defect] PWM needs two additional words (extends ticks.res)
+    v.push_back(0) ;
+    v.push_back(0) ;
     return v ;
 }
 
@@ -56,12 +65,12 @@ std::vector<uint32_t> Device::Ws2812b::BitStream::
 make(Ticks const &ticks,uint32_t grb,size_t n)
 {
     BitStream bs(ticks) ;
-    bs.pushBit(0,ticks.res) ; // [todo] do we need this?
+    bs.pushBit(0,ticks.res) ;
+    // ...the datasheet doesn't really say how to start a dialogue.
+    // The data line has to be low for a little time t before the
+    // transmission can start: 2 pixel <= t <= t.res (by observation).
+    // Anyway, with t.res we have to be on the safe side.
     bs.push(grb,n) ;
     bs.pushBit(0,ticks.res) ;
-    bs.pushBit(0,32) ; // for RPTL [todo] why again?
-    auto size = bs.q.size() ;
-    auto rup = ~0x1fu & (size + 0x1fu) ;
-    bs.pushBit(0,rup-size) ;
     return bs.copy() ;
 }
